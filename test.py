37afb49db8a3e4eb1c8f57ac4c285c4c4bf7555c
Mustafa Hasanbulli <mustafa@hasanbul.li>
2020-04-28 07:00:05 +1200

    Update README.md

--- test.py
+++ test.py
@@ -3,6 +3,5 @@ from at_ml import dataset, lof_lgbm
 d=dataset()
 m=lof_lgbm()
 
-# X_train, X_test, y_train, y_test = d.get_data()
-# m.train(X_train, y_train, X_test, y_test)
-m.predict_results(data_predict)
+X_train, X_test, y_train, y_test = d.get_data()
+m.train(X_train, y_train, X_test, y_test)
